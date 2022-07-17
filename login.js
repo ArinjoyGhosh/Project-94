a116c2c6e8d9d79f00969a77f84003c245a36664
ArinjoyGhosh <91753320+ArinjoyGhosh@users.noreply.github.com>
2022-07-17 22:44:34 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhMCacShqZVr8S-QAbSDXqInr0pMLmhss",
+  authDomain: "don-bosco-liluah---commu-5c5be.firebaseapp.com",
+  databaseURL: "https://don-bosco-liluah---commu-5c5be-default-rtdb.firebaseio.com",
+  projectId: "don-bosco-liluah---commu-5c5be",
+  storageBucket: "don-bosco-liluah---commu-5c5be.appspot.com",
+  messagingSenderId: "213983938148",
+  appId: "1:213983938148:web:7fcb64f1e57eaf7ac88171"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function addUser() {
+  console.log("entered");
+  user_name = document.getElementById("user_name").value;
+  firebase.database().ref("/").child(user_name).update({
+      purpose : "adding user"
+  });
+}
